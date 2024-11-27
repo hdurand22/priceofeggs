e8f8c11107f8faba32e243327aa21516e809efc7
hdurand22 <hdurand22@gmail.com>
2024-11-27 12:31:19 -0800

    created config.js

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const apiBaseUrl = process.env.apiBaseUrl;
+const oauth2BaseUrl = process.env.oauth2BaseUrl;
+const clientId = process.env.clientId;
+const redirectUrl = process.env.redirectUrl;
+
+export default { apiBaseUrl, oauth2BaseUrl, clientId, redirectUrl };
