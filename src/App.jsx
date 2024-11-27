101a7b273a314530d460c710273d4d0f4dc7036a
hdurand22 <hdurand22@gmail.com>
2024-11-27 13:59:59 -0800

    connected to auth URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
 import { useState, useEffect } from 'react'
+import { oauth2BaseUrl, clientId, redirectUrl, scope, clientSecret } from './config'
 import './App.css'
 
 function App() {
-  const [products, setProducts] = useState([]);
+  const [response, setResponse] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    let accessToken = authentication.getAccessToken();
-    fetch(locationUrl, {
-      method: "GET",
-      cache: "no-cache",
-      headers: {
+    authorize();
+  }, [])
 
-      }
-    })
-  })
+  async function authorize() {
+    try {
+      const response = await fetch(`${oauth2BaseUrl}/authorize?scope=${scope}&response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}`, {
+        headers: {
+          "Cache-Control": "no-cache",
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      setResponse(response);
+      console.log(response);
+      setLoading(false);
+    } catch (error) {
+    setError(error);
+    setLoading(false);
+    }
+  }
+
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
+        success
       </div>
     </>
   )
--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-const apiBaseUrl = process.env.apiBaseUrl;
-const oauth2BaseUrl = process.env.oauth2BaseUrl;
-const clientId = process.env.clientId;
-const redirectUrl = process.env.redirectUrl;
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const oauth2BaseUrl = import.meta.env.VITE_OAUTH2_BASE_URL;
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+const redirectUrl = import.meta.env.VITE_REDIRECT_URL;
+const scope = import.meta.env.VITE_SCOPE;
 
-export default { apiBaseUrl, oauth2BaseUrl, clientId, redirectUrl };
+export { apiBaseUrl, oauth2BaseUrl, clientId, clientSecret, redirectUrl, scope };
